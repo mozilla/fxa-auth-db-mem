8ceaf6151fd65ce8902552a95f51a167ea5d0362
Danny Coates <dannycoates@gmail.com>
2014-07-23 11:43:56 -0700

    export the server

--- index.js
+++ index.js
@@ -4,14 +4,27 @@
 
 var api = require('fxa-auth-db-server')
 var DB = require('./db')(api.errors)
-var port = +(process.env['HTTPDB_PORT']) || 8000
-var host = process.env['HTTPDB_HOST'] || '127.0.0.1'
 
-DB.connect().done(
- function (db) {
-   var server = api.createServer(db)
-   server.listen(port, host)
-   server.on('failure', function (d) { console.error(d.err.code, d.url)})
-   server.on('success', function (d) { console.log(d.method, d.url)})
- }
-)
+if (require.main === module) {
+  var port = +(process.env['HTTPDB_PORT']) || 8000
+  var host = process.env['HTTPDB_HOST'] || '127.0.0.1'
+
+  DB.connect().done(
+   function (db) {
+     var server = api.createServer(db)
+     server.listen(port, host)
+     server.on('failure', function (d) { console.error(d.err.code, d.url)})
+     server.on('success', function (d) { console.log(d.method, d.url)})
+   }
+  )
+}
+else {
+  module.exports = function () {
+    return DB.connect()
+      .then(
+        function (db) {
+          return api.createServer(db)
+        }
+      )
+  }
+}
