7b84cc8d8f87834b7fed30bcb3c0678a28914504
Andrew Chilton <andychilton@gmail.com>
2014-10-29 09:38:23 +1300

    Move db_tests.js into test/backend

--- test/backend/db_tests.js
+++ test/backend/db_tests.js
@@ -4,7 +4,7 @@
 
 require('ass')
 var dbServer = require('fxa-auth-db-server')
-var dbTests = require('fxa-auth-db-server/test').dbTests
+var dbTests = require('fxa-auth-db-server/test/backend').dbTests
 var log = { trace: console.log, error: console.log, stat: console.log, info: console.log }
 var DB = require('../../db')(log, dbServer.errors)
 
